f5df51ee3c8fa2fba1d98952fac1f33f73eacd29
Kirill <kirill.shakhov14@gmail.com>
2021-08-02 12:45:02 +0300

    add builder

--- src/main/java/com/ventus/core/models/Account.java
+++ src/main/java/com/ventus/core/models/Account.java
@@ -1,9 +1,11 @@
 package com.ventus.core.models;
 
 import com.ventus.core.interfaces.IAccount;
+import lombok.Builder;
 import lombok.Data;
 
 @Data
+@Builder
 public class Account implements IAccount {
     private String login;
     private String pass;

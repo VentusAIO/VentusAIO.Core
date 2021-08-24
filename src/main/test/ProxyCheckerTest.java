cb0fe7cb8101351a9e0649104fd79e2c02407ee0
Kirill <kirill.shakhov14@gmail.com>
2021-08-24 19:38:22 +0300

    refactor ProxyChecker
    (cherry picked from commit 7c063c0589ec7dcf3a4e983cbff21cb2cb2fb10e)

--- src/main/test/ProxyCheckerTest.java
+++ src/main/test/ProxyCheckerTest.java
@@ -1,32 +1,41 @@
 import com.ventus.core.interfaces.IProxy;
 import com.ventus.core.models.Proxy;
 import com.ventus.core.proxy.ProxyChecker;
+import org.junit.Before;
 import org.junit.Test;
 
 import java.util.LinkedList;
 import java.util.List;
 
 public class ProxyCheckerTest {
-    @Test
-    public void doCheckProxyForAdidas() {
-        //before
-        List<IProxy> proxies = new LinkedList<>();
-        proxies.add(new Proxy("176.53.176.42", 1337, "savvasiry_gmail_com", "b001060fbf"));
-        proxies.add(new Proxy("176.53.166.42", 30001, "savvasiry_gmail_com", "b001060fbf"));
-        //then
-        ProxyChecker.check(proxies, "https://www.adidas.ru");
-        //penis
-        proxies.add(new Proxy("176.53.176.42", 1337, "savvasiry_gmail_com", "b001060fbf"));
+
+    @Before
+    public void configureProxy(){
+        //Настройка прокси
+        System.setProperty("jdk.http.auth.tunneling.disabledSchemes", "");
+        System.setProperty("jdk.http.auth.proxying.disabledSchemes", "");
     }
 
-    @Test
-    public void doCheckProxyForOzon() {
-        //before
-        List<IProxy> proxies = new LinkedList<>();
-        proxies.add(new Proxy("176.53.166.42", 30001, "savvasiry_gmail_com", "b001060fbf"));
-        proxies.add(new Proxy("176.53.166.42", 30001, "savvasiry_gmail_com", "b001060fbf"));
-        //then
-        ProxyChecker.check(proxies, "https://www.ozon.ru");
-        //penis
-    }
+//    @Test
+//    public void doCheckProxyForAdidas() {
+//        //before
+//        List<IProxy> proxies = new LinkedList<>();
+//        proxies.add(new Proxy("176.53.176.42", 1337, "savvasiry_gmail_com", "b001060fbf"));
+//        proxies.add(new Proxy("176.53.166.42", 30001, "savvasiry_gmail_com", "b001060fbf"));
+//        //then
+//        ProxyChecker.check(proxies, "https://www.adidas.ru");
+//        //penis
+//        proxies.add(new Proxy("176.53.176.42", 1337, "savvasiry_gmail_com", "b001060fbf"));
+//    }
+//
+//    @Test
+//    public void doCheckProxyForOzon() {
+//        //before
+//        List<IProxy> proxies = new LinkedList<>();
+//        proxies.add(new Proxy("176.53.166.42", 30001, "savvasiry_gmail_com", "b001060fbf"));
+//        proxies.add(new Proxy("176.53.166.42", 30001, "savvasiry_gmail_com", "b001060fbf"));
+//        //then
+//        ProxyChecker.check(proxies, "https://www.ozon.ru");
+//        //penis
+//    }
 }

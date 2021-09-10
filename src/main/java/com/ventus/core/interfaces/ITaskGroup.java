d0dee805ae348c4da4bea233d5ee17883ffd3581
Kirill <kirill.shakhov14@gmail.com>
2021-09-10 04:26:22 +0300

    fix tests class

--- src/main/java/com/ventus/core/interfaces/ITaskGroup.java
+++ src/main/java/com/ventus/core/interfaces/ITaskGroup.java
@@ -30,45 +30,6 @@ public interface ITaskGroup {
     void setExecutorService(ExecutorService executorService);
 
     List<? extends ITask> start();
-//    default List<ITask> start() {
-//        System.setProperty("jdk.http.auth.tunneling.disabledSchemes", "");
-//        System.setProperty("jdk.http.auth.proxying.disabledSchemes", "");
-//        getExecutorService().shutdownNow();
-//        setExecutorService(Executors.newCachedThreadPool());
-//
-//        ProxyManager proxyManager = new ProxyManager();
-//        proxyManager.addProxyList(getProxies());
-//
-//        ProfileManager profileManager = null;
-//        AccountManager accountManager = null;
-//        if (getProfiles() != null) {
-//            profileManager = new ProfileManager(getProfiles());
-//        }
-//        if (getAccounts() != null) {
-//            accountManager = new AccountManager(getAccounts());
-//        }
-//
-//        List<ITask> task_list = new LinkedList<>();
-//        for (int i = 0; i < getAmount(); i++) {
-//            RequestModule task = TasksFactory.getTask(getTasksType());
-//
-//
-//            task.configureProxy(proxyManager);
-//
-//            if (profileManager != null) task.setProfileManger(profileManager);
-//            if (accountManager != null) task.setAccountManger(accountManager);
-//
-//            task.setItemId(getItemId());
-//            task.setFilter(getFilter());
-//            task.setSizes(getSizes());
-//            task.setUser_logs(itask.getLogs());
-//            Future<Map<?, ?>> future = getExecutorService().submit(task);
-//            itask.setFuture(future);
-//            task_list.add(itask);
-//        }
-//        getExecutorService().shutdown();
-//        return task_list;
-//    }
 
     default void stop() {
         getExecutorService().shutdownNow();
--- src/main/java/com/ventus/core/models/TaskGroup.java
+++ src/main/java/com/ventus/core/models/TaskGroup.java
@@ -2,16 +2,25 @@ package com.ventus.core.models;
 
 import com.ventus.core.interfaces.*;
 import com.ventus.core.network.AvailabilityFilters;
+import com.ventus.core.proxy.ProxyManager;
 import com.ventus.core.task.RequestModule;
+import com.ventus.core.task.TasksFactory;
 import lombok.Data;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 
 @Data
 public class TaskGroup implements ITaskGroup {
+    Class<? extends RequestModule> tasksType = null;
+    AvailabilityFilters filter = null;
+    TaskGroupStatus status;
+    ExecutorService executorService = Executors.newCachedThreadPool();
     private Long id;
     private String name;
     private String itemId;
@@ -19,10 +28,6 @@ public class TaskGroup implements ITaskGroup {
     private List<IProfile> profiles = new ArrayList<>();
     private List<IProxy> proxies = new ArrayList<>();
     private List<IAccount> accounts = new ArrayList<>();
-    Class<? extends RequestModule> tasksType = null;
-    AvailabilityFilters filter = null;
-    TaskGroupStatus status;
-    ExecutorService executorService = Executors.newCachedThreadPool();
 
     @Override
     public String[] getSizes() {
@@ -31,6 +36,43 @@ public class TaskGroup implements ITaskGroup {
 
     @Override
     public List<ITask> start() {
-        return null;
+        System.setProperty("jdk.http.auth.tunneling.disabledSchemes", "");
+        System.setProperty("jdk.http.auth.proxying.disabledSchemes", "");
+        getExecutorService().shutdownNow();
+        setExecutorService(Executors.newCachedThreadPool());
+
+        ProxyManager proxyManager = new ProxyManager();
+        proxyManager.addProxyList(getProxies());
+
+        ProfileManager profileManager = null;
+        AccountManager accountManager = null;
+        if (getProfiles() != null) {
+            profileManager = new ProfileManager(getProfiles());
+        }
+        if (getAccounts() != null) {
+            accountManager = new AccountManager(getAccounts());
+        }
+
+        List<ITask> task_list = new LinkedList<>();
+        for (int i = 0; i < getAmount(); i++) {
+            RequestModule task = TasksFactory.getTask(getTasksType());
+
+
+            task.configureProxy(proxyManager);
+
+            if (profileManager != null) task.setProfileManger(profileManager);
+            if (accountManager != null) task.setAccountManger(accountManager);
+
+            ITask itask = new Task();
+            task.setItemId(getItemId());
+            task.setFilter(getFilter());
+            task.setSizes(getSizes());
+            task.setUser_logs(itask.getLogs());
+            Future<Map<?, ?>> future = getExecutorService().submit(task);
+            itask.setFuture(future);
+            task_list.add(itask);
+        }
+        getExecutorService().shutdown();
+        return task_list;
     }
 }
